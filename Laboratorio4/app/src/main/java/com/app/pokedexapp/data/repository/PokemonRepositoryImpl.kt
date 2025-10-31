77eb7f92239fba99ab565be88b09ab3be0871da3
Jorge Oyoqui <a01711783@tec.mx>
2025-10-30 18:21:54 -0600

    Laboratorio 4

---
+++ Laboratorio4/app/src/main/java/com/app/pokedexapp/data/repository/PokemonRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package com.app.pokedexapp.data.repository
+
+import com.app.pokedexapp.data.mapper.toDomain
+import com.app.pokedexapp.data.remote.api.PokemonApi
+import com.app.pokedexapp.domain.model.Pokemon
+import com.app.pokedexapp.domain.repository.PokemonRepository
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+
+@Singleton
+class PokemonRepositoryImpl
+    @Inject
+    constructor(
+        private val api: PokemonApi,
+    ) : PokemonRepository {
+        override suspend fun getPokemonList(): List<Pokemon> {
+            val response = api.getPokemonList()
+            return response.results.map { result ->
+                // Obtenemos el id de la URL
+                val id =
+                    result.url
+                        .split("/")
+                        .dropLast(1)
+                        .last()
+                // Hacemos la llamada para obtener detalles
+                api.getPokemon(id).toDomain()
+            }
+        }
+
+        override suspend fun getPokemonById(id: String): Pokemon = api.getPokemon(id).toDomain()
+    }
---
+++ Laboratorio4/app/src/main/java/com/app/pokedexapp/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.app.pokedexapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.app.pokedexapp.presentation.navigation.PokemonNavGraph
+import com.app.pokedexapp.ui.theme.PokedexAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PokedexAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PokemonNavGraph(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
