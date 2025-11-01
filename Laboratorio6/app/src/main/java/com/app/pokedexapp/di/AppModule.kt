a66431a7dd37c83a928e270af552db4743e147dc
Jorge Oyoqui <a01711783@tec.mx>
2025-10-31 20:53:53 -0600

    Laboratorio 6

--- Laboratorio6/app/src/main/java/com/app/pokedexapp/di/AppModule.kt
+++ Laboratorio6/app/src/main/java/com/app/pokedexapp/di/AppModule.kt
@@ -1,11 +1,15 @@
 package com.app.pokedexapp.di
 
+import android.content.Context
+import com.app.pokedexapp.data.local.preferences.PokemonPreferences
 import com.app.pokedexapp.data.remote.api.PokemonApi
 import com.app.pokedexapp.data.repository.PokemonRepositoryImpl
 import com.app.pokedexapp.domain.repository.PokemonRepository
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -26,9 +30,23 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun provideGson(): Gson = Gson()
+
+    @Provides
+    @Singleton
     fun providePokemonApi(retrofit: Retrofit): PokemonApi = retrofit.create(PokemonApi::class.java)
 
     @Provides
     @Singleton
-    fun providePokemonRepository(api: PokemonApi): PokemonRepository = PokemonRepositoryImpl(api)
+    fun providePokemonPreferences(
+        @ApplicationContext context: Context,
+        gson: Gson,
+    ): PokemonPreferences = PokemonPreferences(context, gson)
+
+    @Provides
+    @Singleton
+    fun providePokemonRepository(
+        api: PokemonApi,
+        preferences: PokemonPreferences,
+    ): PokemonRepository = PokemonRepositoryImpl(api, preferences)
 }
